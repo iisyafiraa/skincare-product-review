c5d62034f778a34fd9d754823bd25535379e9fae
iisyafiraa <iisyafiraa.yan@gmail.com>
2025-02-22 03:57:14 +0700

    add column

--- dags/resources/sql/gold/fact_product_review.sql
+++ dags/resources/sql/gold/fact_product_review.sql
@@ -4,8 +4,10 @@ CREATE TABLE gold.fact_product_review AS
 SELECT
     p.id AS product_id,
     r.id AS review_id,
+    c.id AS category_id,
     r.rating,
     r.thumb,
     r.review_date
-FROM silver.product_transformed p
-JOIN silver.review_transformed r ON p.id = r.product_id;
+FROM silver.review_transformed r
+JOIN silver.product_transformed p ON p.id = r.product_id
+JOIN silver.category_transformed c ON p.category_id = c.id;
